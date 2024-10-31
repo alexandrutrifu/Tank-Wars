81a887e8299fdbbf9d2ef3732af54eee350617c2
Alexandru Trifu-Urzica <alex@Alexandrus-MacBook-Pro.local>
2024-10-31 23:48:43 +0200

    created Terrain class

--- src/lab_m1/tema1/terrain.cpp
+++ src/lab_m1/tema1/terrain.cpp
@@ -1,12 +1,17 @@
 #include "terrain.h"
+#include "transform.h"
+#include "math.h"
 
 #include <vector>
 
 #include "core/engine.h"
-#include "components/simple_scene.h"
+
+#define max(x, y) ((x) >= (y) ? (x) : (y))
+
+using namespace terrain;
 
 
-Mesh* terrain::CreateSquare(
+Mesh* Terrain::CreateSquare(
     const std::string &name,
     glm::vec3 leftBottomCorner,
     float length,
@@ -38,12 +43,34 @@ Mesh* terrain::CreateSquare(
     return square;
 }
 
-float getTerrainY(float x) {
+void Terrain::renderTerrainSquare(int index, glm::vec3 pointA, glm::vec3 pointB, glm::vec3 corner) {
+    float segmentHeight = max(pointA.y, pointB.y);
+    float segmentWidth = pointB.x - pointA.x;
+
+    // Create standard square
+    Mesh* square = CreateSquare("square" + index, corner, segmentWidth, glm::vec3(1, 0, 0), true);
+    AddMeshToList(square);
+
+    // (1) Scaling
+    modelMatrix = glm::mat3(1);
+    modelMatrix *= transform::Scale(1, segmentHeight);
+
+    // (2) Shearing
+    float shearFactor = (pointB.y - pointA.y) / (pointB.x - pointA.x);
+    modelMatrix *= transform::ShearOY(shearFactor);
+
+    // (3) Translation
+    modelMatrix *= transform::Translate(pointA.x, pointA.y);
+
+    RenderMesh2D(meshes["square" + index], shaders["VertexColor"], modelMatrix);
+}
+
+float Terrain::getTerrainY(float x) {
     return cos(2 * x + 1) - cos(1.5 * x + 1.5) +
          0.2 * sin(3.4 * x + 1) + 2 + x / 20 + sin(0.4 * x);
 }
 
-std::vector<glm::vec3> initializeTerrain(glm::ivec2 resolution) {
+std::vector<glm::vec3> Terrain::getTerrainCoordinates(glm::ivec2 resolution) {
 	std::vector<glm::vec3> terrainCoordinates;
 
     for (int xCoordinate = 0; xCoordinate < resolution.x; xCoordinate += 10) {
@@ -52,4 +79,16 @@ std::vector<glm::vec3> initializeTerrain(glm::ivec2 resolution) {
     }
 
     return terrainCoordinates;
+}
+
+void Terrain::initializeTerrain(glm::ivec2 resolution) {
+    std::vector<glm::vec3> terrainCoordinates = getTerrainCoordinates(resolution);
+    glm::vec3 corner = glm::vec3(0, 0, 0);
+
+    for (int startIndex = 0; startIndex < terrainCoordinates.size() - 1; startIndex++) {
+        glm::vec3 pointA = terrainCoordinates[startIndex];
+        glm::vec3 pointB = terrainCoordinates[startIndex + 1];
+
+        renderTerrainSquare(startIndex, pointA, pointB, corner);
+    }
 }
--- src/lab_m1/tema1/terrain.h
+++ src/lab_m1/tema1/terrain.h
@@ -6,20 +6,36 @@
 #include <string>
 
 #include "core/gpu/mesh.h"
+#include "components/simple_scene.h"
 #include "utils/glm_utils.h"
 
 
 namespace terrain
 {
-    // Create square with given bottom left corner, length and color
-    Mesh *CreateSquare(const std::string &name, glm::vec3 leftBottomCorner,
-                       float length, glm::vec3 color, bool fill = false);
+    class Terrain : public gfxc::SimpleScene {
+        public:
+            Terrain();
+            ~Terrain() override;
 
-	// Get the y coordinate of the terrain at a given x coordinate
-    float getTerrainY(float x);
+            // Create square with given bottom left corner, length and color
+            Mesh *CreateSquare(const std::string &name, glm::vec3 leftBottomCorner,
+                            float length, glm::vec3 color, bool fill = false);
 
-	// Initialize the terrain coordinates array
-	std::vector<glm::vec3> initializeTerrain(glm::ivec2 resolution);
+            // Render a terrain square
+            void renderTerrainSquare(int index, glm::vec3 pointA, glm::vec3 pointB, glm::vec3 corner);
+
+            // Get the y coordinate of the terrain at a given x coordinate
+            float getTerrainY(float x);
+
+            // Initialize the terrain coordinates array
+            std::vector<glm::vec3> getTerrainCoordinates(glm::ivec2 resolution);
+
+            // Initialize terrain
+            void initializeTerrain(glm::ivec2 resolution);
+
+        protected:
+            glm::mat3 modelMatrix{};
+    };
 }
 
 #endif

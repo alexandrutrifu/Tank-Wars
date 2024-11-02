068deb4b7ceae74eaae1816bc22b123df171d5d8
Alexandru Trifu-Urzica <alex@Alexandrus-MacBook-Pro.local>
2024-11-02 15:43:22 +0200

    added skeleton files for drawing tank model

---
+++ src/lab_m1/tema1/tank/tank.h
@@ -0,0 +1,11 @@
+#include "../transform.h"
+#include "math.h"
+
+#include <vector>
+#include <string>
+#include <iostream>
+
+#include "core/engine.h"
+#include "core/gpu/mesh.h"
+#include "components/simple_scene.h"
+#include "utils/glm_utils.h"

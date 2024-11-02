e7737632b0e6c91b02304cf97f2e6800a69a5e54
Alexandru Trifu-Urzica <alex@Alexandrus-MacBook-Pro.local>
2024-11-02 18:47:10 +0200

    created Tank class and CreateTankModel()

--- src/lab_m1/tema1/tank/tank.cpp
+++ src/lab_m1/tema1/tank/tank.cpp
@@ -1,2 +1,31 @@
 #include "tank.h"
 
+
+const glm::vec3 tank_body_colour = glm::vec3(0.796, 0.686, 0.533);
+
+tanks::Tank::Tank() = default;
+
+tanks::Tank::~Tank() = default;
+
+tanks::Tank tanks::Tank::CreateTankModel(const std::string &name, glm::vec3 leftBottomCorner) {
+    tanks::Tank tank{};
+    
+    // Create tank body
+    Mesh *tankBody = objects::CreateTrapezoid(name + "_body", leftBottomCorner,
+                TANK_BODY_LENGTH, TANK_BODY_HEIGHT, tank_body_colour, true);
+
+    // Create tank dome
+    Mesh *tankDome = objects::CreateDiskHalf(name + "_dome", TANK_DOME_RADIUS,
+                DISK_VERTEX_COUNT,
+                leftBottomCorner + glm::vec3(DOME_CENTER_X, DOME_CENTER_Y, 0),
+                tank_body_colour, true);
+
+    tank.tankParts.push_back(tankBody);
+    tank.tankParts.push_back(tankDome);
+
+    return tank;
+}
+
+std::vector<Mesh *> tanks::Tank::getTankParts() const {
+    return tankParts;
+}
--- src/lab_m1/tema1/tank/tank.h
+++ src/lab_m1/tema1/tank/tank.h
@@ -1,4 +1,10 @@
+#ifndef TANK_H
+#define TANK_H
+
+#pragma once
+
 #include "../transform.h"
+#include "../objects/objects.h"
 #include "math.h"
 
 #include <vector>
@@ -9,3 +15,32 @@
 #include "core/gpu/mesh.h"
 #include "components/simple_scene.h"
 #include "utils/glm_utils.h"
+
+#define TANK_BODY_LENGTH 400
+#define TANK_BODY_HEIGHT 70
+#define TANK_DOME_RADIUS 80
+#define TANK_TURRET_LENGTH 200
+
+#define DOME_CENTER_X (TANK_BODY_LENGTH / 2)
+#define DOME_CENTER_Y (TANK_BODY_HEIGHT * 0.9)
+
+extern const glm::vec3 tank_body_colour;
+
+namespace tanks {
+    class Tank {
+        public:
+            Tank();
+            ~Tank();
+
+            // Returns a vector of meshes that form the tank model
+            static tanks::Tank CreateTankModel(const std::string &name, glm::vec3 leftBottomCorner);
+
+            // Get the tank parts
+            std::vector<Mesh *> getTankParts() const;
+
+        private:
+            std::vector<Mesh *> tankParts{};
+    };
+}
+
+#endif
